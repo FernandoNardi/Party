19f591e3a97a371e9f50ca3df0cf461df36e45ec
Nardi <fernandonardi87@gmail.com>
2017-10-23 22:59:22 -0200

    Layout application. Udemy developer course kotlin - application android.

---
+++ app/src/main/java/br/nardi/party/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.nardi.party
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setIcon(R.mipmap.ic_launcher)
+    }
+
+}
